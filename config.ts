6bb44f7af23be2f934fa11196c351d34efab872d
thrustvectorlabs <thrustvectorlabs@protonmail.com>
2025-10-19 21:58:22 +0200

    Added configuration files again, to be able to deploy.

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  SERVER_TYPE: 'rust',
+  SERVER_HOST: '136.243.18.104',
+  SERVER_PORT: 28017,
+  INTERVAL: 0, // Set to nonzero for repeated queries
+};
---
+++ packages/server/src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+
+  webSocket: {
+    port: 9123,
+  },
+  webServer: {
+    port: 3000,
+  },
+}
