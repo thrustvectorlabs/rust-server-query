473d115644244264a23f66f0bc048966ccfe41e4
thrustvectorlabs <thrustvectorlabs@protonmail.com>
2026-01-11 00:42:38 +0100

    production=false (for testing)

--- update-and-deploy.sh
+++ update-and-deploy.sh
@@ -18,6 +18,9 @@ fi
 nvm use
 git pull --ff-only
 
+# Force install of all dependencies (including dev) during deploy.
+export YARN_PRODUCTION=false
+
 run_in_dir() {
   local dir="$1"; shift
   echo "Running in: $dir"
@@ -26,8 +29,8 @@ run_in_dir() {
 
 cp config.ts config.js
 
-run_in_dir "." yarn install
-run_in_dir "packages/web" yarn install
+run_in_dir "." yarn install --production=false
+run_in_dir "packages/web" yarn install --production=false
 
 if [ -f "packages/web/.env" ]; then
   set -a
@@ -37,7 +40,7 @@ fi
 
 run_in_dir "packages/web" yarn build
 run_in_dir "packages/web" ./rsync-to-nginx.sh
-run_in_dir "packages/server" yarn install
+run_in_dir "packages/server" yarn install --production=false
 run_in_dir "packages/server" ./kill-server.sh
 run_in_dir "packages/server" yarn build
 run_in_dir "packages/server" ./run-server-in-background.sh
